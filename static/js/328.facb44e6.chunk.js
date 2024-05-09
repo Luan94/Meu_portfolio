20976be24929a6ed96dd7dbdc13483eb2928c256
Luan94 <luanvilas0@gmail.com>
2024-05-09 13:11:25 -0300

    Updates

--- static/js/328.facb44e6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmeu_portfolio=self.webpackChunkmeu_portfolio||[]).push([[328],{328:(n,o,t)=>{t.r(o),t.d(o,{default:()=>u});var e,i=t(312),a=(t(60),t(360)),r=t(496);const s=a.cp.div(e||(e=(0,i.c)(["\n  width: 100%;\n  height: 8vh;\n  margin-top: -8vh;\n  position: absolute;\n  background: linear-gradient(to bottom, rgba(0, 0, 0, 0),#000000);\n\n"]))),u=()=>(0,r.jsx)(s,{})}}]);
-//# sourceMappingURL=328.facb44e6.chunk.js.map
